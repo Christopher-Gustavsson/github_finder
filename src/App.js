6cacf65a3959d1e4138fae60cfc45d5c5ddc3480
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-07-06 08:48:37 +0000

    user's page now also d displays 5 of their repos

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component{
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -56,6 +57,19 @@ class App extends Component{
     })
   }
 
+  getUserRepos = async username =>{
+    this.setState({
+      loading: true
+    });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:sc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`);
+    
+    this.setState({
+      repos: res.data,
+      loading: false
+    }) 
+  }
+
   clearUsers = () => {
     this.setState({
       users: [],
@@ -79,7 +93,7 @@ class App extends Component{
   }
 
   render(){
-    const {users, user, loading} = this.state;
+    const {users, user, repos, loading} = this.state;
 
     return(
       <Router>
@@ -106,7 +120,9 @@ class App extends Component{
             <User 
               {...props} 
               getUser={this.getUser} 
+              getUserRepos={this.getUserRepos}
               user={user} 
+              repos={repos}
               loading={ loading}/>
             )}  
           />
